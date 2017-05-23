79ef1d5a5d3acbd536d2e263f861dc2ee18b5fd0
Omar S <osanseviero@gmail.com>
2017-05-23 12:14:13 -0500

    feat: Manual calculation of parameters and shape of convolutional layer

--- CNN/conv-dims/conv-dims.py
+++ CNN/conv-dims/conv-dims.py
@@ -1,7 +1,23 @@
+from math import ceil
 from keras.models import Sequential
 from keras.layers import Conv2D
 
 model = Sequential()
-model.add(Conv2D(filters=16, kernel_size=2, strides=2, padding="valid",
-	activation="relu", input_shape=(200, 200, 1)))
+model.add(Conv2D(filters=32, kernel_size=3, strides=2, padding="same",
+	activation="relu", input_shape=(128, 128, 3)))
 model.summary()
+
+# Parameters = K * Weights + K = K * F * F * D_in + K
+number_parameters = 32 * 3 * 3 * 3 + 32
+
+# Shape of convolutional layer
+
+# Depth = K
+depth = 32
+
+# Since it is a same padding: height=ceil(H_in/S)
+height = ceil(float(128)/float(2))
+width = ceil(float(128)/float(2))
+
+print("Number of parameters: ", number_parameters)
+print("Shape of Convolutional layer (%d,%d,%d)" % (height, width, depth))
